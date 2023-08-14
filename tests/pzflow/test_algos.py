c2ffc3c6c4ce62ccfa6faf3557383cb8df3d06c3
drewoldag <47493171+drewoldag@users.noreply.github.com>
2023-08-14 13:56:13 -0700

    Adding unit tests.

--- tests/pzflow/test_algos.py
+++ tests/pzflow/test_algos.py
@@ -13,13 +13,7 @@ DS = RailStage.data_store
 DS.__class__.allow_overwrite = True
 
 
-@pytest.mark.parametrize(
-    "inputs",
-    [
-        (False, [0.15, 0.14, 0.11, 0.14, 0.12, 0.14, 0.15, 0.16, 0.11, 0.12]),
-        (True, [0.15, 0.14, 0.15, 0.14, 0.12, 0.14, 0.15, 0.12, 0.13, 0.11]),
-    ],
-)
+@pytest.mark.parametrize("inputs", [False, True])
 @pytest.mark.slow
 def test_pzflow(inputs):
     def_bands = ["u", "g", "r", "i", "z", "y"]
@@ -61,7 +55,7 @@ def test_pzflow(inputs):
     estim_config_dict = dict(
         hdf5_groupname="photometry",
         model="PZflowPDF.pkl",
-        calculated_point_estimates=['mode'],
+        calculated_point_estimates=['mode', 'mean', 'median'],
     )
 
     # zb_expected = np.array([0.15, 0.14, 0.11, 0.14, 0.12, 0.14, 0.15, 0.16, 0.11, 0.12])
@@ -74,3 +68,59 @@ def test_pzflow(inputs):
     # slightly different answers for python3.7 vs python3.8 for some reason
     #    assert np.isclose(results.ancil['mode'], zb_expected, atol=0.05).all()
     assert np.isclose(results.ancil["mode"], rerun_results.ancil["mode"], atol=0.05).all()
+    assert np.isclose(results.ancil["mean"], rerun_results.ancil["mean"], atol=0.05).all()
+    assert np.isclose(results.ancil["median"], rerun_results.ancil["median"], atol=0.05).all()
+
+@pytest.mark.slow
+def test_pzflow_no_point_estimates():
+    """Verify that the estimation machinery works correctly when we do not request
+    calculated point estimates.
+    """
+    def_bands = ["u", "g", "r", "i", "z", "y"]
+    refcols = [f"mag_{band}_lsst" for band in def_bands]
+    def_maglims = dict(
+        mag_u_lsst=27.79,
+        mag_g_lsst=29.04,
+        mag_r_lsst=29.06,
+        mag_i_lsst=28.62,
+        mag_z_lsst=27.98,
+        mag_y_lsst=27.05,
+    )
+    def_errnames = dict(
+        mag_err_u_lsst="mag_u_lsst_err",
+        mag_err_g_lsst="mag_g_lsst_err",
+        mag_err_r_lsst="mag_r_lsst_err",
+        mag_err_i_lsst="mag_i_lsst_err",
+        mag_err_z_lsst="mag_z_lsst_err",
+        mag_err_y_lsst="mag_y_lsst_err",
+    )
+    train_config_dict = dict(
+        zmin=0.0,
+        zmax=3.0,
+        nzbins=301,
+        flow_seed=0,
+        ref_column_name="mag_i_lsst",
+        column_names=refcols,
+        mag_limits=def_maglims,
+        include_mag_errors=False,
+        error_names_dict=def_errnames,
+        n_error_samples=3,
+        soft_sharpness=10,
+        soft_idx_col=0,
+        redshift_column_name="redshift",
+        num_training_epochs=50,
+        hdf5_groupname="photometry",
+        model="PZflowPDF.pkl",
+    )
+    estim_config_dict = dict(
+        hdf5_groupname="photometry",
+        model="PZflowPDF.pkl"
+    )
+
+    train_algo = pzflow_nf.PZFlowInformer
+    pz_algo = pzflow_nf.PZFlowEstimator
+    results, _, _ = one_algo(
+        "PZFlow", train_algo, pz_algo, train_config_dict, estim_config_dict
+    )
+
+    assert results.ancil is None
