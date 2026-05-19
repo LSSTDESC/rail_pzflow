b7369c15a4ff54b3f5e41bbee69a176b978ba85b
hangqianjun <hangqianjun007@gmail.com>
2026-05-19 08:11:41 -0700

    testing and debugging

--- src/rail/creation/degraders/pzflow_degrader.py
+++ src/rail/creation/degraders/pzflow_degrader.py
@@ -2,13 +2,14 @@
 
 import pandas as pd
 from ceci.config import StageParameter as Param
-
+from pzflow import Flow
 from rail.core.data import PqHandle
 from rail.creation.noisifier import Noisifier
 from rail.tools.flow_handle import FlowHandle
+from collections import OrderedDict
 
 _def_bands = ["u", "g", "r", "i", "z", "y"]
-_def_mag_col_template = "mag_{band}_lsst"
+_def_mag_col_template = {band: f"mag_{band}_lsst" for band in _def_bands}
 _def_error_col_map = {band: f"mag_{band}_lsst_err" for band in _def_bands}
 
 class PZFlowNoisifier(Noisifier):
@@ -27,26 +28,27 @@ class PZFlowNoisifier(Noisifier):
 
     config_options = Noisifier.config_options.copy()
     config_options.update(
-        bands=Param(
-            list,
-            _def_bands,
-            msg="Ordered list of photometric band letters (must match the order the flow was trained on).",
-        ),
-        mag_col_template=Param(
-            str,
+        mag_col_template = Param(
+            OrderedDict,
             _def_mag_col_template,
-            msg="Template for magnitude column names in the input catalog. Use {band} as placeholder.",
+            msg=(
+                "Mapping from magnitude-specific columns to data magnitude column name. "
+                "Default: {'u': 'mag_u_lsst', ...}."
+            )
         ),
-        additional_cols=Param(
-            list,
-            [],
-            msg="Additional columns used as conditions, e.g. 5-sigma depth, must be in the same order as the model.",
+        conditional_col_map=Param(
+            dict,
+            {},
+            msg=(
+                "Additional mapping from conditional columns in the model to data columns. "
+                "Default: {'u_depth': 'u_depth', ...}."
+            )
         ),
         error_col_map=Param(
-            dict,s
+            OrderedDict,
             _def_error_col_map,
             msg=(
-                "Mapping from band letter to output error column name. "
+                "Mapping from band to output error column name. "
                 "Keys must be single-character band letters (e.g. 'u', 'g'). "
                 "Default: {'u': 'mag_u_lsst_err', 'g': 'mag_g_lsst_err', ...}."
             ),
@@ -70,11 +72,16 @@ class PZFlowNoisifier(Noisifier):
         data = self.get_data("input")
         data_df = pd.DataFrame(data)
 
-        bands = self.config.bands
-        mag_cols = [self.config.mag_col_template.format(band=b) for b in bands]
-        if len(self.config.additional_cols)>0:
-            mag_cols += self.config.additional_cols
-        conditions = data_df[mag_cols]
+        flow_cols = self.noiseModel.conditional_columns
+        conditional_cols = []
+        for col in flow_cols:
+            if col in (self.config.mag_col_template).keys():
+                conditional_cols.append(self.config.mag_col_template[col])
+            elif col in (self.config.conditional_col_map).keys():
+                conditional_cols.append(self.config.conditional_col_map[col])
+            else:
+                raise ValueError(f"Column: {col} required by the model is not provided.")
+        conditions = data_df[conditional_cols]
 
         samples = self.noiseModel.sample(
             nsamples=1,
@@ -84,14 +91,14 @@ class PZFlowNoisifier(Noisifier):
         )
 
         # now add errors on the magnitude:
-        flow_cols = self.noiseModel.data_columns
-        for band, model_col in zip(bands, flow_cols):
-            data_df[f'mag_{band}_lsst'] += samples[model_col]
+        for mag_col, mag_err_col in zip(self.config.mag_col_template.keys(), self.noiseModel.data_columns):
+            band = self.config.mag_col_template[mag_col]
+            data_df[band] = data_df[band] + samples[mag_err_col]
 
         # decorreation:
         if self.config.decorrelate == True:
             # update conditions
-            conditions = data_df[mag_cols]
+            conditions = data_df[conditional_cols]
             # recompute error
             samples = self.noiseModel.sample(
             nsamples=1,
@@ -102,13 +109,9 @@ class PZFlowNoisifier(Noisifier):
         
         # The flow's data_columns are assumed to correspond to bands in the same order
         # as self.config.bands. Rename them to the desired output column names.
-        rename_map = {
-            flow_col: self.config.error_col_map[band]
-            for flow_col, band in zip(flow_cols, bands)
-        }
-        samples = samples.rename(columns=rename_map)
+        cols_to_keep = list(self.config.error_col_map.keys())
+        samples = samples[cols_to_keep]
+        samples = samples.rename(columns=self.config.error_col_map)
         samples.index = data_df.index
-
-        error_cols = [self.config.error_col_map[b] for b in bands]
-        output_df = pd.concat([data_df, samples[error_cols]], axis=1)
+        output_df = pd.concat([data_df, samples], axis=1)
         self.add_data("output", output_df)
